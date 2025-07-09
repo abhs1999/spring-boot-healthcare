d434d0474c858130de25e8f0cc9fbdc8b53fd0aa
abhs99 <abhilash@gmail.com>
2025-07-09 10:55:32 +0530

    Initial commit - Spring Boot Microservice

---
+++ com.abc.springboot-microservice-1.0.0/src/main/java/com/abc/microservice/HealthController.java
@@ -0,0 +1,14 @@
+package com.abc.microservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public String healthCheck() {
+        return "Service is UP!";
+    }
+}
+
